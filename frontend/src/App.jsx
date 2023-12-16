6654ef20783d3797cc1f550937c58891b8b164ce
Steven <salinas.steve536@gmail.com>
2023-12-16 11:59:04 -0800

    authme phases 0-3 complete. STILL GETTING LOGIN INTERNAL ERROR

---
+++ frontend/src/App.jsx
@@ -0,0 +1,48 @@
+// frontend/src/App.jsx
+
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import SignupFormPage from './components/SignupFormPage';
+import Navigation from './components/Navigation';
+import * as sessionActions from './store/session';
+
+function Layout() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => {
+      setIsLoaded(true)
+    });
+  }, [dispatch]);
+
+  return (
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && <Outlet />}
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <h1>Welcome!</h1>
+      },
+      {
+        path: "signup",
+        element: <SignupFormPage />
+      }
+    ]
+  }
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
