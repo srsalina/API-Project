8d6c71c3da36e98094ae1eec51df25ed5a03fc37
Steven <salinas.steve536@gmail.com>
2023-12-21 11:23:31 -0800

    cleanup

--- frontend/src/components/ManageSpots/ManageSpots.jsx
+++ frontend/src/components/ManageSpots/ManageSpots.jsx
@@ -1,26 +1,26 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { thunkGetAllSpots } from "../../store/spots";
-import { NavLink } from "react-router-dom";
-import OpenModalButton from "../OpenModalButton/OpenModalButton";
-import DeleteSpot from "../DeleteSpot/DeleteSpot";
-import './ManageSpots.css'
+// import { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { thunkGetAllSpots } from "../../store/spots";
+// import { NavLink } from "react-router-dom";
+// import OpenModalButton from "../OpenModalButton/OpenModalButton";
+// import DeleteSpot from "../DeleteSpot/DeleteSpot";
+// import './ManageSpots.css'
 
 
-export default function ManageSpots(){
+// export default function ManageSpots(){
 
-    const dispatch = useDispatch()
+//     const dispatch = useDispatch()
 
-    const spots = useSelector((state) => state.spots.allSpots.Spots)
+//     const spots = useSelector((state) => state.spots.allSpots.Spots)
 
-    console.log(spots)
+//     console.log(spots)
 
-    if(!spots){
-        dispatch(thunkGetAllSpots())
-        return null
-    }
+//     if(!spots){
+//         dispatch(thunkGetAllSpots())
+//         return null
+//     }
 
-    useEffect(() =>{
-        dispatch(thunkGetAllSpots())
-    }, dispatch)
-}
+//     useEffect(() =>{
+//         dispatch(thunkGetAllSpots())
+//     }, dispatch)
+// }
--- frontend/src/components/UpdateSpot/UpdateSpot.jsx
+++ frontend/src/components/UpdateSpot/UpdateSpot.jsx
@@ -1,32 +1,32 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { thunkSpotDetails, thunkSpotUpdate } from "../../store/spots";
-import './UpdateSpot.css'
+// import { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useParams } from "react-router-dom";
+// import { thunkSpotDetails, thunkSpotUpdate } from "../../store/spots";
+// import './UpdateSpot.css'
 
 
-export default function UpdateSpot(){
-    const dispatch = useDispatch()
-    const {spotId} = useParams()
-    const spot = useSelector((state) => state.spots.currSpot)
-    console.log(spot)
+// export default function UpdateSpot(){
+//     const dispatch = useDispatch()
+//     const {spotId} = useParams()
+//     const spot = useSelector((state) => state.spots.currSpot)
+//     console.log(spot)
 
-    const [ address, setAddress ] = useState(spot.address)
-    const [ city, setCity ] = useState(spot.city)
-    const [ state, setState ] = useState(spot.state)
-    const [ country, setCountry ] = useState(spot.country)
-    const [ lat, setLat ] = useState(spot.lat)
-    const [ lng, setLng ] = useState(spot.lng)
-    const [ name, setName ] = useState(spot.name)
-    const [ description, setDescription ] = useState(spot.description)
-    const [ price, setPrice ] = useState(spot.price)
-    const [ avgRating, setAvgRating ] = useState(spot.avgRating)
-    const [ previewImage, setPreviewImage ] = useState(spot.previewImage)
-
-
-    useEffect(() => {
-        dispatch(thunkSpotDetails(spotId))
-    }, [dispatch,spotId])
+//     const [ address, setAddress ] = useState(spot.address)
+//     const [ city, setCity ] = useState(spot.city)
+//     const [ state, setState ] = useState(spot.state)
+//     const [ country, setCountry ] = useState(spot.country)
+//     const [ lat, setLat ] = useState(spot.lat)
+//     const [ lng, setLng ] = useState(spot.lng)
+//     const [ name, setName ] = useState(spot.name)
+//     const [ description, setDescription ] = useState(spot.description)
+//     const [ price, setPrice ] = useState(spot.price)
+//     const [ avgRating, setAvgRating ] = useState(spot.avgRating)
+//     const [ previewImage, setPreviewImage ] = useState(spot.previewImage)
 
 
-}
+//     useEffect(() => {
+//         dispatch(thunkSpotDetails(spotId))
+//     }, [dispatch,spotId])
+
+
+// }
