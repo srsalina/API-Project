d5d6591921880baa5962aa6b4c85979bf094beb1
Steven <salinas.steve536@gmail.com>
2024-01-01 17:26:58 -0800

    here we go

--- backend/db/seeders/20231031005326-demo-spotImages.js
+++ backend/db/seeders/20231031005326-demo-spotImages.js
@@ -23,7 +23,108 @@ module.exports = {
         spotId: 3,
         url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-645734927755358154/original/ee26e961-bd79-4423-8791-df80f8c7382c.jpeg?im_w=1200',
         preview: true
-      }
+      },
+      {
+        spotId: 4,
+        url: 'https://shorturl.at/bJL07',
+        preview: true
+      },
+      {
+        spotId: 4,
+        url: 'https://shorturl.at/bJL07',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://shorturl.at/bJL07',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://shorturl.at/bJL07',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://shorturl.at/bJL07',
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: 'https://shorturl.at/dmOQ0',
+        preview: true
+      },
+      {
+        spotId: 5,
+        url: 'https://shorturl.at/dmOQ0',
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: 'https://shorturl.at/dmOQ0',
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: 'https://shorturl.at/dmOQ0',
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: 'https://shorturl.at/dmOQ0',
+        preview: false
+      },
+      {
+        spotId: 6,
+        url: 'https://shorturl.at/hit56',
+        preview: true
+      },
+      {
+        spotId: 6,
+        url: 'https://shorturl.at/hit56',
+        preview: false
+      },
+      {
+        spotId: 6,
+        url: 'https://shorturl.at/hit56',
+        preview: false
+      },
+      {
+        spotId: 6,
+        url: 'https://shorturl.at/hit56',
+        preview: false
+      },
+      {
+        spotId: 6,
+        url: 'https://shorturl.at/hit56',
+        preview: false
+      },
+      { spotId: 7, url: 'https://shorturl.at/begAQ', preview: true },
+      { spotId: 7, url: 'https://shorturl.at/begAQ', preview: false },
+      { spotId: 7, url: 'https://shorturl.at/begAQ', preview: false },
+      { spotId: 7, url: 'https://shorturl.at/begAQ', preview: false },
+      { spotId: 7, url: 'https://shorturl.at/begAQ', preview: false },
+      { spotId: 7, url: 'https://shorturl.at/begAQ', preview: false },
+      { spotId: 8, url: 'https://shorturl.at/dnrHM', preview: true },
+      { spotId: 8, url: 'https://shorturl.at/dnrHM', preview: false },
+      { spotId: 8, url: 'https://shorturl.at/dnrHM', preview: false },
+      { spotId: 8, url: 'https://shorturl.at/dnrHM', preview: false },
+      { spotId: 8, url: 'https://shorturl.at/dnrHM', preview: false },
+      { spotId: 8, url: 'https://shorturl.at/dnrHM', preview: false },
+
+      { spotId: 9, url: 'https://shorturl.at/lqERV', preview: true },
+      { spotId: 9, url: 'https://shorturl.at/lqERV', preview: false },
+      { spotId: 9, url: 'https://shorturl.at/lqERV', preview: false },
+      { spotId: 9, url: 'https://shorturl.at/lqERV', preview: false },
+      { spotId: 9, url: 'https://shorturl.at/lqERV', preview: false },
+      { spotId: 9, url: 'https://shorturl.at/lqERV', preview: false },
+
+      { spotId: 10, url: 'https://shorturl.at/dnAEY', preview: true },
+      { spotId: 10, url: 'https://shorturl.at/dnAEY', preview: false },
+      { spotId: 10, url: 'https://shorturl.at/dnAEY', preview: false },
+      { spotId: 10, url: 'https://shorturl.at/dnAEY', preview: false },
+      { spotId: 10, url: 'https://shorturl.at/dnAEY', preview: false },
+      { spotId: 10, url: 'https://shorturl.at/dnAEY', preview: false },
     ], { validate: true })
   },
 
